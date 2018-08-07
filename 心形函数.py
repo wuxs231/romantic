8ad76cac19e2ffb9ba5641ac42ccfb40feec8299
Lancer <wuxs231@163.com>
2018-08-07 11:25:34 +0800

    Add files via upload

---
+++ 心形函数.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
+
+x = np.linspace(-2, 2, 200)
+y1 = np.sqrt(1-np.square(np.fabs(x)-1))
+y2 = np.arccos(1-np.fabs(x))-np.pi
+
+plt.plot(x, y1, 'r', x, y2, 'r')
+plt.axis([-2.5, 2.5, -3.5, 1.5])
+
+plt.title('coded by Lancer Wu', fontsize=16)
+plt.show()
